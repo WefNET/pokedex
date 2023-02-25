33556d2007fca637331d108d6fca764f3ae45cb5
WefNET <john.wefler@gmail.com>
2023-02-25 11:53:37 -0600

    Card update

---
+++ frontend-app/src/components/poke-card.tsx
@@ -0,0 +1,32 @@
+import { Card, CardMedia, CardContent, Typography, CardActions, Button } from "@mui/material";
+
+const PokeCard = (props: any) => {
+	console.log('Poke in Card: ', props);
+
+	const pokemon = props.pokemon;
+	const species = props.species;
+
+	const pokeText = species.flavor_text_entries[0].flavor_text;
+
+	return (
+		<>
+			<Card sx={{ maxWidth: 345 }}>
+				<CardMedia
+					sx={{ height: 350 }}
+					image={pokemon.sprites.other.home.front_default}
+					title={pokemon.name}
+				/>
+				<CardContent>
+					<Typography gutterBottom variant="h5" component="div">
+						{pokemon.name}
+					</Typography>
+					<Typography variant="body2" color="text.secondary">
+						{pokeText}
+					</Typography>
+				</CardContent>
+			</Card>
+		</>
+	)
+}
+
+export default PokeCard;
